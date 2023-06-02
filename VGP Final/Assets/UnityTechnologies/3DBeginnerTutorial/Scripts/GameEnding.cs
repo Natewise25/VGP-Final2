b94c026f767e7f3c835516a15400aa1f18d233d2
Natewise25 <113540259+Natewise25@users.noreply.github.com>
2023-06-02 08:41:59 -0700

    Copied Project

---
+++ VGP Final/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public GameObject player;
+    public float displayImageDuration = 1f;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+    float m_timer;
+    bool m_IsPlayerCaught;
+    
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+    
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+            {
+            EndLevel (exitBackgroundImageCanvasGroup, false);
+            }  
+            else if (m_IsPlayerCaught)
+            {
+            EndLevel (caughtBackgroundImageCanvasGroup, true);
+            }
+    }
+                                                    
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
+    {
+        m_timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_timer / fadeDuration;
+        
+        if(m_timer > fadeDuration + displayImageDuration)
+        {
+            if(doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit ();
+            }
+        }
+    }
+}    
