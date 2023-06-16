287c540cfe799c364f9f80a1d2bdb244ac681f3f
Natewise25 <113540259+Natewise25@users.noreply.github.com>
2023-06-16 08:59:43 -0700

    Yes

--- VGP Final/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
+++ VGP Final/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
@@ -6,14 +6,18 @@ using UnityEngine.SceneManagement;
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
-    public GameObject player;
     public float displayImageDuration = 1f;
+    public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
 
     bool m_IsPlayerAtExit;
-    float m_timer;
     bool m_IsPlayerCaught;
+    float m_timer;
+    bool m_HasAudioPlayed;
+   
     
     void OnTriggerEnter (Collider other)
     {
@@ -32,16 +36,22 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)
             {
-            EndLevel (exitBackgroundImageCanvasGroup, false);
+            EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
             }  
             else if (m_IsPlayerCaught)
             {
-            EndLevel (caughtBackgroundImageCanvasGroup, true);
+            EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
             }
     }
                                                     
-    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
     {
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
         m_timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_timer / fadeDuration;
         
